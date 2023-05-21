fef4b9e3c99425a15547449db4ae7c7e06096c94
elabedamna <elabed.aminaaa@gmail.com>
2023-05-21 09:37:16 +0100

    k

--- src/model/MyDesktopPlanner.java
+++ src/model/MyDesktopPlanner.java
@@ -16,31 +16,7 @@ import java.util.Map.Entry;
 
 import com.sun.javafx.collections.MappingChange.Map;
 
-/*public class MyDesktopPlanner extends Application {
-    private User userModel;
 
-    @Override
-    public void start(Stage primaryStage) {
-        // Create a User object with some tasks
-        User user = new User();
-        user.setUsername("Amina");
-        ArrayList<Task> tasks = new ArrayList<>();
-        tasks.add(new Task("Make dinner", 60, PrioTask.MEDIUM, null, false, Status.TODO, "Category 1"));
-        tasks.add(new Task("Task 2", 120, PrioTask.HIGH, null, false, Status.TODO, "Category 2"));
-        tasks.add(new Task("Task 3", 30, PrioTask.LOW, null, false, Status.TODO, "Category 3"));
-        user.setTasks(tasks);
-
-        // Create the TaskView and show it
-        /*TaskView taskView = new TaskView(user);
-        taskView.show(primaryStage);
-        WelcomePageView welcome = new WelcomePageView();
-        welcome.show(primaryStage);
-    }
-
-    public static void main(String[] args) {
-        launch(args);
-    }
-}*/
 //Main class
 public class MyDesktopPlanner extends Application {
 
