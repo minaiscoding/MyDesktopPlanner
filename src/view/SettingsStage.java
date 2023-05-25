88d53f18c23a512409433e274aa3137dd59d237e
minaiscoding <khadir04kh@gmail.com>
2023-05-25 09:30:13 +0100

    settings

--- src/view/SettingsStage.java
+++ src/view/SettingsStage.java
@@ -4,7 +4,9 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.AppData;
 import model.HistoryData;
@@ -44,6 +46,66 @@ public class SettingsStage extends Stage {
         });
 
         // Add button actions here
+     // Handle "Change minimal duration of timeslot" button click
+        changeMinDurationButton.setOnAction(event -> {
+            // Create a new stage for entering the minimal duration of timeslot
+            Stage changeDurationStage = new Stage();
+            changeDurationStage.initModality(Modality.APPLICATION_MODAL);
+            changeDurationStage.setResizable(false);
+
+            VBox changeDurationRoot = new VBox(20);
+            changeDurationRoot.setAlignment(Pos.CENTER);
+
+            // Create the label and text field for entering the duration
+            Label durationLabel = new Label("Enter minimal duration of timeslot (in minutes):");
+            TextField durationTextField = new TextField();
+            durationTextField.setPromptText("Enter a number");
+
+            // Create the save button
+            Button saveButton = new Button("Save");
+            saveButton.setOnAction(saveEvent -> {
+                String durationText = durationTextField.getText();
+                if (!durationText.isEmpty()) {
+                	appData.getCurrentUser().getPlanner().setMinTaskPerDay(Integer.parseInt(durationText));
+                }
+                changeDurationStage.close();
+            });
+
+            changeDurationRoot.getChildren().addAll(durationLabel, durationTextField, saveButton);
+            Scene changeDurationScene = new Scene(changeDurationRoot, 350, 150);
+            changeDurationStage.setScene(changeDurationScene);
+            changeDurationStage.show();
+        });
+     // Handle "Change minimal number of tasks" button click
+        changeMinTasksButton.setOnAction(event -> {
+            // Create a new stage for entering the minimal number of tasks
+            Stage changeTasksStage = new Stage();
+            changeTasksStage.initModality(Modality.APPLICATION_MODAL);
+            changeTasksStage.setResizable(false);
+
+            VBox changeTasksRoot = new VBox(20);
+            changeTasksRoot.setAlignment(Pos.CENTER);
+
+            // Create the label and text field for entering the number of tasks
+            Label tasksLabel = new Label("Enter minimal number of tasks:");
+            TextField tasksTextField = new TextField();
+            tasksTextField.setPromptText("Enter a number");
+
+            // Create the save button
+            Button saveButton = new Button("Save");
+            saveButton.setOnAction(saveEvent -> {
+                String tasksText = tasksTextField.getText();
+                if (!tasksText.isEmpty()) {
+                    appData.getCurrentUser().getPlanner().setMinTaskPerDay(Integer.parseInt(tasksText));
+                }
+                changeTasksStage.close();
+            });
+
+            changeTasksRoot.getChildren().addAll(tasksLabel, tasksTextField, saveButton);
+            Scene changeTasksScene = new Scene(changeTasksRoot, 300, 150);
+            changeTasksStage.setScene(changeTasksScene);
+            changeTasksStage.show();
+        });
 
         root.getChildren().addAll(titleLabel, changeMinTasksButton, addCategoryButton,
                 changeMinDurationButton, saveAndStartButton);
